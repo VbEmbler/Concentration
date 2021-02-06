12699beb8f8dc1ea0a4b386cfb42c3b61275dacc
Vladimir Bondarenko <vbembler@gmail.com>
2021-02-06 18:58:03 +0300

    CA-26 lecture 3

--- Concentration/Card.swift
+++ Concentration/Card.swift
@@ -10,21 +10,22 @@ import Foundation
 
 struct Card {
     
-    //MARK: - Static Properties
-    static var identifierFactory = 0
     
     //MARK: - Public Properties
     var isFaceUp = false
     var isMatched = false
     var identifier: Int
     
+    //MARK: - Private Properties
+    private static var identifierFactory = 0
+    
     //MARK: - Initializers
     init() {
         self.identifier = Card.getUniqueIdentifier()
     }
     
-    //MARK: - Static Methods
-    static func getUniqueIdentifier() -> Int {
+    //MARK: - Private Methods
+    private static func getUniqueIdentifier() -> Int {
         identifierFactory += 1
         return identifierFactory
     }
--- Concentration/Concentration.swift
+++ Concentration/Concentration.swift
@@ -10,12 +10,32 @@ import Foundation
 
 class Concentration {
     
-    //MARK: - Public Properties
-    var cards: [Card] = []
-    var indexOfOneAndOnlyOneFaceUpCard: Int?
+    //MARK: - Private Properties
+    private(set) var cards: [Card] = []
+    private var indexOfOneAndOnlyOneFaceUpCard: Int? {
+        get {
+            var foundIndex: Int?
+            for index in cards.indices {
+                if cards[index].isFaceUp {
+                    if foundIndex == nil {
+                        foundIndex = index
+                    } else {
+                        return nil
+                    }
+                }
+            }
+            return foundIndex
+        }
+        set {
+            for index in cards.indices {
+                cards[index].isFaceUp = (index == newValue)
+            }
+        }
+    }
     
     //MARK: - Initializers
     init(numberOfPairsOfCards: Int) {
+        assert(numberOfPairsOfCards > 0, "Concentration.init\(numberOfPairsOfCards)) : you must have at least one pair of cards")
         for _ in 1...numberOfPairsOfCards {
             let card = Card()
             cards += [card, card]
@@ -25,6 +45,7 @@ class Concentration {
     
     //MARK: - Public Methods
     func chooseCard(at index: Int) {
+        assert(cards.indices.contains(index), "Concentration.chooseCard(at: \(index)): chossen index not in the cards")
         if !cards[index].isMatched {
             if let matchIndex = indexOfOneAndOnlyOneFaceUpCard, matchIndex != index {
                 if cards[index].identifier == cards[matchIndex].identifier {
@@ -32,13 +53,8 @@ class Concentration {
                     cards[matchIndex].isMatched = true
                 }
                 cards[index].isFaceUp = true
-                indexOfOneAndOnlyOneFaceUpCard = nil
             } else {
                 //either no cards or 2 cards are face up
-                for flipDownIndex in cards.indices {
-                    cards[flipDownIndex].isFaceUp = false
-                }
-                cards[index].isFaceUp = true
                 indexOfOneAndOnlyOneFaceUpCard = index
             }
         }
